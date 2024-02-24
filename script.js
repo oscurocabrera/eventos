cd248fca06225a9c42504156ba5031d80ef0480e
oscurocabrera <oscurocabrera@gmail.com>
2024-02-24 17:19:03 -0600

    eventos villareal

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("ESTAS APUNTO DE ENTRAR A LA MEJOR PAGINA DE EVENTOS");
+function zoomIn(element) {
+    element.classList.add('zoomed');
+}
+
+function zoomOut(element) {
+    element.classList.remove('zoomed');
+}
